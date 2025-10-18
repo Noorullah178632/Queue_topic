7bc3547ca194d1562b6f1362a7f2566c7bb4fc0c
NoorUllah <nooullah178632@gmail.com>
2025-10-18 22:39:13 +0500

    circular queue

---
+++ circular_queue.dart
@@ -0,0 +1,13 @@
+class CircularQueue {
+  int maxSize = 5;
+  late List<int> data;
+  int front = -1;
+  int rear = -1;
+  CircularQueue() {
+    data = List.filled(maxSize, 0);
+  }
+  //enQueue
+  void enQueue(int value) {}
+}
+
+void main() {}
